fa3a8c410df5e13575ef5b73681a7a47ac76d47d
Tony Byrne <tonyb@byrnehq.com>
2015-04-13 21:46:09 +0100

    Oopsie!

--- enigma.rb
+++ enigma.rb
@@ -1,8 +1,8 @@
 module Enigma
   ALPHABET  = Array('A'..'Z').freeze
 
-  ROTOR_I_MAP   = %w(E K M F L G D Q V Z N T O W Y H X U S P A I B R C J).freeze,
-  ROTOR_II_MAP  = %w(A J D K S I R U X B L H W T M C Q G Z N P Y F V O E).freeze,
+  ROTOR_I_MAP   = %w(E K M F L G D Q V Z N T O W Y H X U S P A I B R C J).freeze
+  ROTOR_II_MAP  = %w(A J D K S I R U X B L H W T M C Q G Z N P Y F V O E).freeze
   ROTOR_III_MAP = %w(B D F H J L C P R T X V Z N Y E I W G A K M U S Q O).freeze
 
   ROTOR_I_NOTCH   = 'R'
