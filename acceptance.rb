ceeccae07fa10cea991a4e4b1e2d6f5aa815de21
Tony Byrne <tonyb@byrnehq.com>
2015-04-14 21:36:38 +0100

    Added challenge to find the key.

--- acceptance.rb
+++ acceptance.rb
@@ -4,3 +4,11 @@ require_relative 'enigma'
 
 puts "The Super-Secret Text is:"
 puts Enigma::Machine.new.convert('JZNUZQLGKALLRBETZOGQUPSBXGT')
+
+machine = Enigma::Machine.new
+machine.rotors[2].set '?' # Left Rotor
+machine.rotors[1].set '?' # Middle Rotor
+machine.rotors[0].set '?' # Right Motor
+
+puts "The Super-Secret Text is:"
+puts machine.convert('SOHGLRJHIRYGIMZJRN')
