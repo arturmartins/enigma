9cec6508c2da994d421a76ed0b0d857d6ad732a1
Tony Byrne <tonyb@byrnehq.com>
2015-04-14 21:38:46 +0100

    Comments to aid development.

--- acceptance.rb
+++ acceptance.rb
@@ -2,13 +2,15 @@
 
 require_relative 'enigma'
 
-puts "The Super-Secret Text is:"
+# Uses defaults where rotor starting postions are A-A-A
+puts "The First Super-Secret Text is:"
 puts Enigma::Machine.new.convert('JZNUZQLGKALLRBETZOGQUPSBXGT')
 
+# Find the rotor starting positions that allow the following to be decrypted
 machine = Enigma::Machine.new
 machine.rotors[2].set '?' # Left Rotor
 machine.rotors[1].set '?' # Middle Rotor
 machine.rotors[0].set '?' # Right Motor
 
-puts "The Super-Secret Text is:"
+puts "The Second Super-Secret Text is:"
 puts machine.convert('SOHGLRJHIRYGIMZJRN')
