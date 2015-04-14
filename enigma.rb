b1342699ec34b054cf09f36b852b20e2612e85ba
Tony Byrne <tonyb@byrnehq.com>
2015-04-14 15:56:52 +0100

    Checkpoint!

--- enigma.rb
+++ enigma.rb
@@ -1,22 +1,51 @@
 module Enigma
-  ALPHABET  = Array('A'..'Z').freeze
+  ALPHABET = Array('A'..'Z').freeze
 
-  ROTOR_I_MAP   = %w(E K M F L G D Q V Z N T O W Y H X U S P A I B R C J).freeze
-  ROTOR_II_MAP  = %w(A J D K S I R U X B L H W T M C Q G Z N P Y F V O E).freeze
+  ROTOR_I_MAP = %w(E K M F L G D Q V Z N T O W Y H X U S P A I B R C J).freeze
+  ROTOR_II_MAP = %w(A J D K S I R U X B L H W T M C Q G Z N P Y F V O E).freeze
   ROTOR_III_MAP = %w(B D F H J L C P R T X V Z N Y E I W G A K M U S Q O).freeze
 
-  ROTOR_I_NOTCH   = 'R'
-  ROTOR_II_NOTCH  = 'F'
+  ROTOR_I_NOTCH = 'R'
+  ROTOR_II_NOTCH = 'F'
   ROTOR_III_NOTCH = 'W'
 
   REFLECTOR_B_MAP = %w(Y R U H Q S L D P X N G O K M I E B F Z C W V J A T).freeze
 
   class Machine
+    def convert(char)
+    end
+  end
 
-    def decrypt(ciphertext)
-      # Implement Me :-)
+  class Rotor
+
+    attr_reader :offset
+
+    def initialize(mapping)
+      @mapping = mapping
+      @offset = 0
     end
 
+    def set(char)
+      @offset = ALPHABET.index(char)
+    end
+
+    def window_char
+      ALPHABET[@offset]
+    end
+
+    def rotate!
+      @offset = (@offset + 1) % ALPHABET.length
+    end
+
+    def forward(char, offset=0)
+      i = ALPHABET.index(char)
+      @mapping.rotate(@offset - offset)[i]
+    end
+
+    def reverse(char, offset=0)
+      i = @mapping.rotate(@offset - offset).index(char)
+      ALPHABET[i]
+    end
   end
 
 end
--- enigma_spec.rb
+++ enigma_spec.rb
@@ -1,5 +1,72 @@
 require_relative 'enigma'
 
 describe Enigma do
+
+  describe Enigma::Rotor do
+
+    let(:rotor_iii) { Enigma::Rotor.new(Enigma::ROTOR_III_MAP) }
+    let(:rotor_ii)  { Enigma::Rotor.new(Enigma::ROTOR_II_MAP) }
+
+    it 'should start with "A" as the window char' do
+      expect(rotor_iii.window_char).to eql 'A'
+    end
+
+    it 'should rotate 1 step so that "B" is the window character' do
+      rotor_iii.rotate!
+      expect(rotor_iii.window_char).to eql 'B'
+    end
+
+    it 'should rotate back to "A" when position is "Z"' do
+      rotor_iii.set('Z')
+      rotor_iii.rotate!
+      expect(rotor_iii.window_char).to eql 'A'
+    end
+
+    it 'rotor III should forward map "A" to "B"' do
+      expect(rotor_iii.forward('A')).to eql 'B'
+    end
+
+    it 'rotor III rotated 1 step should forward map "A" to "D"' do
+      rotor_iii.rotate!
+      expect(rotor_iii.forward('A')).to eql 'D'
+    end
+
+    it 'rotor III rotated 1 step should reverse map "D" to "A"' do
+      rotor_iii.rotate!
+      expect(rotor_iii.reverse('D')).to eql 'A'
+    end
+
+    it 'rotor III set to "Z" and rotated 1 step should forward map "A" to "O"' do
+      rotor_iii.set('Z')
+      expect(rotor_iii.forward('A')).to eql 'O'
+    end
+
+    it 'rotor III should reverse map "B" to "A"' do
+      expect(rotor_iii.reverse('B')).to eql 'A'
+    end
+
+    it 'rotors III and II at defaults should forward map "A" to "J"' do
+      char = rotor_iii.forward('A')
+      expect(rotor_ii.forward(char)).to eql 'J'
+    end
+
+    it 'rotors III and II at defaults should reverse map "J" to "A"' do
+      char = rotor_ii.reverse('J')
+      expect(rotor_iii.reverse(char)).to eql 'A'
+    end
+
+    it 'rotors III and II with III set to "B" should forward map "A" to "D"' do
+      rotor_iii.set('B')
+      char = rotor_iii.forward('A')
+      expect(rotor_ii.forward(char, rotor_iii.offset)).to eql 'D'
+    end
+
+    it 'rotors III and II with III set to "B" should reverse map "D" to "A"' do
+      rotor_iii.set('B')
+      char = rotor_ii.reverse('D',rotor_iii.offset)
+      expect(rotor_iii.reverse(char)).to eql 'A'
+    end
+  end
+
 end
 
